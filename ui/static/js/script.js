03bde4294cf7a27c8f208bf2bf359601249ab78d
Risana Kamalakaran <risanakamalakaran@risanas-macbook-pro.home>
2020-05-13 15:16:26 +0200

    js file has been separated successfully

---
+++ ui/static/js/script.js
@@ -0,0 +1,204 @@
+let token = null;
+let deviceList = null;
+let csvFile = null;
+$(document).ready(function() {
+    $("#callGo").on('click', function() {
+        $("#tenantList").removeClass("hidden");
+        loginInfo = {
+            "login": "rkamalakaran.ext@objenious.com",
+            "password": "Michael1508"
+        }
+        $.ajax({
+            url: "https://api-preprod.objenious.com/v2/login",
+            contentType: 'application/json',
+            method: "POST",
+            data: JSON.stringify(loginInfo),
+            success: function(data) {
+                token = data.token;
+                $.ajax({
+                    url: "http://api-preprod.objenious.com/v2/user/profile",
+                    method: "GET",
+                    headers: {
+                        "x-token": data.token
+                    },
+                    success: function(tenantList) {
+                        tenantList.contexts.forEach(element => {
+                            $("#tenantList")
+                                .append($("<option></option>")
+                                .attr("value",element.id)
+                                .text(element.organization_label));
+                        });
+                    },
+                })
+            },
+        });
+    });
+});
+function tenantListSelect (index, choosenTenant) {
+    var paragraph = document.getElementById("choosenTenant");
+    var text = document.createTextNode(choosenTenant);
+    paragraph.appendChild(text);
+
+    $.ajax({
+        url: "https://api-preprod.objenious.com/v2/user/profile/chooseContext/" + index,
+        method: "POST",
+        headers: {
+            "x-token": token
+        },
+        success: function(tenant) {
+            $.ajax({
+                url: "https://api-preprod.objenious.com/v2/devices",
+                method: "GET",
+                headers: {
+                    "x-token": token
+                },
+                success: function (tenantDevices) {
+                    deviceList = tenantDevices;
+                    var table = $('#tenantDevice');
+                    table.find("thead tr")[0].style.visibility = "visible";
+                    table.find("tbody tr").remove();
+                    var header = table.find("thead tr#tableHeader")
+                    header.append("<th>ID</th><th>Name</th><th>Description</th>")
+                    var propertiesKeys = [];
+                    tenantDevices.forEach(function (elt) {
+                        Object.keys(elt.properties).forEach(function (head) {
+                            propertiesKeys.push(head);
+                        })
+                    });
+                    var unique = propertiesKeys.filter(function(elem, index, self) {
+                        return index === self.indexOf(elem);
+                    })
+                    unique.forEach(function(tablehead) {
+                        header.append("<th>" + tablehead + "</th>")
+                    })
+                    tenantDevices.forEach(function (elt2) {
+                        table.append("<tr id='" + elt2.id + "'><td>" + elt2.id + "</td><td>" + elt2.name + "</td><td>" + elt2.description + "</td></tr>");
+                        unique.forEach(function (key) {
+                            var row = document.getElementById(elt2.id);
+                            var x = row.insertCell(-1);
+                            x.innerHTML= "<td>" + elt2.properties[key] + "</td>";
+                        })
+                    })
+                }
+            })
+        },
+    })
+}
+function handleFileSelect(evt) {
+    var file = evt.target.files[0];
+    Papa.parse(file, {
+        header: true,
+        dynamicTyping: true,
+        complete: function(results) {
+            data = results;
+            csvFile = data.data;
+            createSendingElement();
+            var table = $('#CSVfileContent');
+            table.find("thead tr")[0].style.visibility = "visible";
+            table.find("tbody tr").remove();
+            var header = table.find("thead tr#tableCSVHeader")
+            
+            var propertiesKeys = [];
+            csvFile.forEach(function (elt) {
+                Object.keys(elt).forEach(function (head) {
+                    propertiesKeys.push(head);
+                })
+            });
+            var unique = propertiesKeys.filter(function(elem, index, self) {
+                return index === self.indexOf(elem);
+            })
+            
+            var p = $('#csvFileContent');
+            p[0].style.visibility = "visible";
+
+            unique.forEach(function (key) {
+                header.append("<th>" + key + "</th>")
+            })
+
+            csvFile.forEach(function (elt2) {
+                table.append("<tr id='csv" + elt2.id + "'></tr>");
+                unique.forEach(function (key) {
+                    var row = document.getElementById('csv' + elt2.id);
+                    var x = row.insertCell(-1);
+                    x.innerHTML= "<td>" + elt2[key] + "</td>";
+                })
+            })
+        }
+    });
+}
+var eltIdentique = [];
+var tab = []
+$(document).ready(function(){
+    $("#csv-file").change(handleFileSelect);
+});
+function createSendingElement() {
+
+    csvFile.forEach(function (elt) {
+        deviceList.forEach(function (elt2) {
+            if(elt.id === elt2.id) {
+                eltIdentique.push(elt.id);
+                console.log('EXISTE');
+            }
+        })
+    })
+
+    eltIdentique.forEach(function (ids) {
+        csvFile.forEach(function (csv) {
+            if(ids === csv['id']) {
+                var result = deviceList.find(obj => {
+                    return obj.id === ids
+                })
+                var sendingObject = {
+                    "group_id" : result.group.id,
+                    'profile_id' : result.profile.id,
+                    "properties" : {}
+                };
+                if ((keyExist = "name" in csv) === true) {
+                    sendingObject.name = csv.name;
+                }
+                if ((keyExist = "description" in csv) === true) {
+                    sendingObject.description = csv.description;
+                }
+                if ((keyExist = "status" in csv) === true) {
+                    sendingObject.status = csv.status;
+                }
+                
+                sendingObject.properties = csv;
+                sendingObject.properties.external_id = result.properties.external_id
+                tab.push(sendingObject);
+            }
+        })
+    })
+    /* $('#existElt').append("L'ID suivant existe dans les deux listes : " + elt.id + "</br>");
+        var sendingElt ={
+            "name": elt2.name,
+            "group_id": elt.group.id,
+            "profile_id": elt.profile.id,
+            "properties": {
+                "external_id": elt.properties.external_id
+            }
+        }
+        console.log('sending element : ', sendingElt);
+        $.ajax({
+            url: "https://api-preprod.objenious.com/v2/devices/" + elt.id,
+            method: "PUT",
+            headers: {
+                "x-token": token,
+            },
+            data: JSON.stringify(sendingElt),
+        }) */
+    // console.log('taille du device list : ', deviceList.length)
+    // console.log('IDENTIQUE = ', identique);
+
+    updateFunction ();
+}
+
+function updateFunction () {
+    /* tab.forEach(function(elt) {
+        delete elt.properties.name
+        delete elt.properties.id
+        delete elt.properties.description
+        delete elt.properties.status
+    }) */
+    console.log('UPDATE', tab, eltIdentique);
+}
